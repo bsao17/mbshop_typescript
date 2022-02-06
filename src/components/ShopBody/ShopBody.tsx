92e710e305ef15b8fd3b851e6faa7852f6ce268b
bsao17 <declic62@gmail.com>
2022-02-06 08:04:41 +0100

    init

---
+++ src/components/ShopBody/ShopBody.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './shopBody.module.scss'
+
+export default function ShopBody(){
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
